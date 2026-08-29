9b8cc78479ad6623c3af3a98e8bc3eddd6a8e393
OM-Coder95 <ommore8855@gmail.com>
2026-08-29 08:08:10 +0530

    read functionality completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,35 @@
 const cl = console.log;
 
+const studentContainer = document.getElementById("studentContainer");
 // Database
 
-let enrollmentArr = [
-  {
-    id: "EN101",
-    course: "Angular",
-    skills: ["HTML", "CSS", "JavaScript", "TypeScript"],
-    joiningDate: "2026-08-10",
-    feeStatus: "Paid",
-  },
+let jsonArr = localStorage.getItem("enrollmentArr");
 
-  {
-    id: "EN102",
-    course: "React",
-    skills: ["HTML", "CSS", "JavaScript"],
-    joiningDate: "2026-08-12",
-    feeStatus: "Pending",
-  },
+let enrollmentArr = jsonArr ? JSON.parse(jsonArr) : [];
 
-  {
-    id: "EN103",
-    course: "Java",
-    skills: ["JavaScript", "Bootstrap"],
-    joiningDate: "2026-08-15",
-    feeStatus: "Paid",
-  },
-];
+// Functions
 
-localStorage.setItem("enrollmentArr", JSON.stringify(enrollmentArr))
+// read
 
+function showOnUI(arr) {
+  let result = "";
 
+  arr.forEach((ele, i) => {
+    result += `
+                                <tr id="${ele.id}">
+                                    <td>${i + 1}</td>
+                                    <td>${ele.course}</td>
+                                    <td>${ele.skills}</td>
+                                    <td>${ele.joiningDate}</td>
+                                    <td>${ele.feeStatus}</td>
+                                    <td class="d-flex justify-content-between">
+                                        <button class="btn btn-sm btn-primary mr-2">Edit</button>
+                                        <button class="btn btn-sm btn-danger">Delete</button>
+                                    </td>
+                                </tr>
+    `;
+  });
+  studentContainer.innerHTML = result;
+}
+
+showOnUI(enrollmentArr);
