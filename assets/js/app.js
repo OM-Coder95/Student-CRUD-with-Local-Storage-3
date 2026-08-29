1a896a7b7b634b1e5bef3d70d9bf29dc439e2a41
OM-Coder95 <ommore8855@gmail.com>
2026-08-29 08:46:55 +0530

    Create functionality completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 const cl = console.log;
 
 const studentContainer = document.getElementById("studentContainer");
+const form = document.getElementById("form");
+const course = document.getElementById("course");
+const joiningDate = document.getElementById("joiningDate");
+
 // Database
 
 let jsonArr = localStorage.getItem("enrollmentArr");
@@ -33,3 +37,44 @@ function showOnUI(arr) {
 }
 
 showOnUI(enrollmentArr);
+
+// Create
+
+function onStudentAdd(event) {
+  event.preventDefault();
+
+  let newStudent = {
+    id: crypto.randomUUID(),
+    course: course.value,
+    skills: [...document.querySelectorAll(`input[name="skills"]:checked`)].map(
+      (ele) => ele.value,
+    ),
+    joiningDate: joiningDate.value,
+    feeStatus: document.querySelector(`input[name="feeStatus"]:checked`).value,
+  };
+
+  enrollmentArr.push(newStudent);
+  localStorage.setItem("enrollmentArr", JSON.stringify(enrollmentArr));
+
+  // UI
+
+  let tr = document.createElement("tr");
+
+  tr.id = newStudent.id;
+
+  tr.innerHTML = `
+                                  <td>${enrollmentArr.length}</td>
+                                    <td>${newStudent.course}</td>
+                                    <td>${newStudent.skills}</td>
+                                    <td>${newStudent.joiningDate}</td>
+                                    <td>${newStudent.feeStatus}</td>
+                                    <td class="d-flex justify-content-between">
+                                        <button class="btn btn-sm btn-primary mr-2">Edit</button>
+                                        <button class="btn btn-sm btn-danger">Delete</button>
+                                    </td>
+  `;
+
+  studentContainer.append(tr);
+}
+
+form.addEventListener("submit", onStudentAdd);
